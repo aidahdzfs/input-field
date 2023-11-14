3ef7abd8778d5569c5b30774090b2eafe2224c5f
aidahdzfs <aidahzulfadilah@gmail.com>
2023-11-15 00:13:41 +0800

    perbaiki fungsi dan menambahkan enterButton

--- src/app/page.js
+++ src/app/page.js
@@ -6,15 +6,20 @@ import './globals.css'
 
 export default function Home() {
 
-  const [input, setInput] = useState('aida')
-  const [nama, setNama] = useState(input)
+  const [input, setInput] = useState('')
+  const [nama, setNama] = useState('aida')
 
 
-  const handlerGantiNama = (val) => {
-    console.warn(val.target.value)
-    setInput(val.target.value)
-    setNama(input)
+  const handlerGantiNama = () => {
+    setNama(input);
+  }
 
+  const handlerInput = (val) => {
+    setInput(val);
+  }
+
+  function enterButton(e) {
+    if (e.code == "Enter") handlerGantiNama();
   }
 
   return (
@@ -29,21 +34,24 @@ export default function Home() {
             />
           </div>
           <div className="nama">
-            {
-              nama?
-              <h1>{input}</h1> :null
-            }
+            <h1>{nama}</h1>
             <div className="bio-nim">
               <p>D121211037</p>
               <p>Teknik Informatika</p>
             </div>
           </div>
         </div>
-        <div className='inputfields'>
-          <input type='text' onChange={handlerGantiNama}/>
-        </div>
-        <div className="cta" onClick={(val)=>setNama(true)}>
-          <button>
+        <div className="cta">
+          <input
+            placeholder='Siapa nama anda?'
+            onInput={(val) => handlerInput(val.target.value)}
+            onKeyDown={(value) => {
+              enterButton(value)
+            }}
+          ></input>
+          <button onClick={() => {
+              handlerGantiNama();
+            }}>
             <p>ganti nama</p>
           </button>
         </div>
