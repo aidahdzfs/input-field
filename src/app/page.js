c4ab9c750d7aef8650528e568f67d8e42f5a8c2a
aidahdzfs <aidahzulfadilah@gmail.com>
2023-11-14 15:43:57 +0800

    membuat konten

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,53 @@
+'use client'
+
+import { useState } from 'react'
 import Image from 'next/image'
-import styles from './page.module.css'
+import './globals.css'
 
 export default function Home() {
+
+  const [input, setInput] = useState('aida')
+  const [nama, setNama] = useState(input)
+
+
+  const handlerGantiNama = (val) => {
+    console.warn(val.target.value)
+    setInput(val.target.value)
+    setNama(input)
+
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <div className='body'>
+      <div className="banner-container">
+        <div className="header-banner-wrapper">
+          <div className="foto">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+            src="/assets/aida.png"
+            fill
+            objectFit='contain'
             />
-          </a>
+          </div>
+          <div className="nama">
+            {
+              nama?
+              <h1>{input}</h1> :null
+            }
+            <div className="bio-nim">
+              <p>D121211037</p>
+              <p>Teknik Informatika</p>
+            </div>
+          </div>
+        </div>
+        <div className='inputfields'>
+          <input type='text' onChange={handlerGantiNama}/>
+        </div>
+        <div className="cta" onClick={(val)=>setNama(true)}>
+          <button>
+            <p>ganti nama</p>
+          </button>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
